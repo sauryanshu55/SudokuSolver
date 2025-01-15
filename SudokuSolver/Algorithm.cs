a235c66a17e090fcc3888a67d6601b9cb7f79395
sauryanshu55 <95371800+sauryanshu55@users.noreply.github.com>
2025-01-15 09:19:21 -0600

    Add project files.

---
+++ SudokuSolver/Algorithm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class GFG
+    {
+
+        public static bool isSafe(int[,] board,
+                                  int row, int col,
+                                  int num)
+        {
+
+            // Row has the unique (row-clash)
+            for (int d = 0; d < board.GetLength(0); d++)
+            {
+
+                // Check if the number
+                // we are trying to
+                // place is already present in
+                // that row, return false;
+                if (board[row, d] == num)
+                {
+                    return false;
+                }
+            }
+
+            // Column has the unique numbers (column-clash)
+            for (int r = 0; r < board.GetLength(0); r++)
+            {
+
+                // Check if the number 
+                // we are trying to
+                // place is already present in
+                // that column, return false;
+                if (board[r, col] == num)
+                {
+                    return false;
+                }
+            }
+
+            // corresponding square has
+            // unique number (box-clash)
+            int sqrt = (int)Math.Sqrt(board.GetLength(0));
+            int boxRowStart = row - row % sqrt;
+            int boxColStart = col - col % sqrt;
+
+            for (int r = boxRowStart;
+                 r < boxRowStart + sqrt; r++)
+            {
+                for (int d = boxColStart;
+                     d < boxColStart + sqrt; d++)
+                {
+                    if (board[r, d] == num)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // if there is no clash, it's safe
+            return true;
+        }
+
+        public static (bool, int[,]) solveSudoku(int[,] board, int n)
+        {
+            int row = -1;
+            int col = -1;
+            bool isEmpty = true;
+
+            // Find an empty cell
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        row = i;
+                        col = j;
+                        isEmpty = false;
+                        break;
+                    }
+                }
+                if (!isEmpty)
+                {
+                    break;
+                }
+            }
+
+            // If no empty cell is found, Sudoku is solved
+            if (isEmpty)
+            {
+                return (true, board);
+            }
+
+            // Try filling numbers 1 to n
+            for (int num = 1; num <= n; num++)
+            {
+                if (isSafe(board, row, col, num))
+                {
+                    board[row, col] = num;
+
+                    var result = solveSudoku(board, n);
+                    if (result.Item1) // If the recursive call succeeded
+                    {
+                        return result;
+                    }
+
+                    // Undo the move (backtracking)
+                    board[row, col] = 0;
+                }
+            }
+
+            // If no number can fit, the Sudoku is unsolvable
+            return (false, board);
+        }
+    }
+}
---
+++ SudokuSolver/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Algorithm;
+
+namespace SudokuSolver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateSudokuGrid();
+        }
+
+        private void CreateSudokuGrid()
+        {
+            // Define the grid size (9x9)
+            int gridSize = 9;
+            SudokuGrid.RowDefinitions.Clear();
+            SudokuGrid.ColumnDefinitions.Clear();
+
+            // Create row and column definitions (9 rows and 9 columns)
+            for (int i = 0; i < gridSize; i++)
+            {
+                SudokuGrid.RowDefinitions.Add(new RowDefinition());
+                SudokuGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            // Add 81 TextBox elements to the grid
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int col = 0; col < gridSize; col++)
+                {
+                    TextBox numberBox = new TextBox
+                    {
+                        Width = 40,
+                        Height = 40,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        MaxLength = 1, // Allow only one character (digit)
+                        Margin = new Thickness(5),
+                        TextAlignment = TextAlignment.Center,
+                        FontSize = 16,
+                    };
+
+                    // Assign the TextBox to the grid cell
+                    Grid.SetRow(numberBox, row);
+                    Grid.SetColumn(numberBox, col);
+                    SudokuGrid.Children.Add(numberBox);
+
+                    // Attach the PreviewTextInput event to restrict input to digits
+                    numberBox.PreviewTextInput += NumberBox_PreviewTextInput;
+                }
+            }
+        }
+
+        private void CollectButton_Click(object sender, RoutedEventArgs e)
+        {
+            int[,] gridData = CollectGridNumbers();
+
+            // Example: Print the grid data to the console
+            for (int row = 0; row < gridData.GetLength(0); row++)
+            {
+                for (int col = 0; col < gridData.GetLength(1); col++)
+                {
+                    Console.Write(gridData[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private int[,] CollectGridNumbers()
+        {
+            int gridSize = 9;
+            int[,] numbers = new int[gridSize, gridSize];
+
+            foreach (UIElement element in SudokuGrid.Children)
+            {
+                if (element is TextBox textBox)
+                {
+                    // Get the row and column of the TextBox
+                    int row = Grid.GetRow(textBox);
+                    int col = Grid.GetColumn(textBox);
+
+                    // Parse the text into an integer, default to 0 if empty or invalid
+                    if (int.TryParse(textBox.Text, out int value))
+                    {
+                        numbers[row, col] = value;
+                    }
+                    else
+                    {
+                        numbers[row, col] = 0; // Treat empty or invalid cells as 0
+                    }
+                }
+            }
+
+
+            (bool, int[,]) solvable = GFG.solveSudoku(numbers, 9);
+
+            return numbers;
+        }
+
+
+        // Method to handle input validation (only allow numeric values)
+        private void NumberBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("^[0-9]$");
+            e.Handled = !regex.IsMatch(e.Text); // Reject non-digit input
+        }
+    }
+}
